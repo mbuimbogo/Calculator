89cbd2940dd3b7456ab7a18ec196cf356d554583
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-09-11 15:14:09 +0300

    Grabbing elements to js

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+// selecting the data attribute use square brackets
+const operationButtons = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const allClearBuuton = document.querySelector("[data-all-clear]")
+const previosOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[ data-current-operand ]")
